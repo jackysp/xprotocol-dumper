d6b9c0360037c5e0878f46f018852c1b4a7c6610
Yu Shuaipeng <jackysp@gmail.com>
2017-08-28 19:41:30 +0800

    update

--- protocol/main.go
+++ protocol/main.go
@@ -7,7 +7,6 @@ import (
 	"io"
 	"os"
 
-	"github.com/golang/protobuf/proto"
 	"github.com/juju/errors"
 	"github.com/pingcap/tipb/go-mysqlx"
 	"github.com/pingcap/tipb/go-mysqlx/Connection"
@@ -48,74 +47,74 @@ func dealClientMsg(mtype Mysqlx.ClientMessages_Type, payload []byte, f *os.File)
 	switch mtype {
 	case Mysqlx.ClientMessages_CON_CAPABILITIES_GET:
 		var data Mysqlx_Connection.CapabilitiesGet
-		proto.Unmarshal(payload, &data)
+		data.Unmarshal(payload)
 		contentS = data.String()
 	case Mysqlx.ClientMessages_CON_CAPABILITIES_SET:
-		var caps Mysqlx_Connection.CapabilitiesSet
-		proto.Unmarshal(payload, &caps)
-		contentS = caps.String()
+		var data Mysqlx_Connection.CapabilitiesSet
+		data.Unmarshal(payload)
+		contentS = data.String()
 	case Mysqlx.ClientMessages_CON_CLOSE:
 		var data Mysqlx_Connection.Close
-		proto.Unmarshal(payload, &data)
+		data.Unmarshal(payload)
 		contentS = data.String()
 	case Mysqlx.ClientMessages_SESS_AUTHENTICATE_START:
-		var authStart Mysqlx_Session.AuthenticateStart
-		proto.Unmarshal(payload, &authStart)
-		contentS = authStart.String()
+		var data Mysqlx_Session.AuthenticateStart
+		data.Unmarshal(payload)
+		contentS = data.String()
 	case Mysqlx.ClientMessages_SESS_AUTHENTICATE_CONTINUE:
-		var authCont Mysqlx_Session.AuthenticateContinue
-		proto.Unmarshal(payload, &authCont)
-		contentS = authCont.String()
+		var data Mysqlx_Session.AuthenticateContinue
+		data.Unmarshal(payload)
+		contentS = data.String()
 	case Mysqlx.ClientMessages_SESS_RESET:
 		var data Mysqlx_Session.Reset
-		proto.Unmarshal(payload, &data)
+		data.Unmarshal(payload)
 		contentS = data.String()
 	case Mysqlx.ClientMessages_SESS_CLOSE:
 		var data Mysqlx_Session.Close
-		proto.Unmarshal(payload, &data)
+		data.Unmarshal(payload)
 		contentS = data.String()
 	case Mysqlx.ClientMessages_SQL_STMT_EXECUTE:
-		var sql Mysqlx_Sql.StmtExecute
-		proto.Unmarshal(payload, &sql)
-		contentS = sql.String()
+		var data Mysqlx_Sql.StmtExecute
+		data.Unmarshal(payload)
+		contentS = data.String()
 	case Mysqlx.ClientMessages_CRUD_FIND:
 		var data Mysqlx_Crud.Find
-		proto.Unmarshal(payload, &data)
+		data.Unmarshal(payload)
 		contentS = data.String()
 	case Mysqlx.ClientMessages_CRUD_INSERT:
 		var data Mysqlx_Crud.Insert
-		proto.Unmarshal(payload, &data)
+		data.Unmarshal(payload)
 		contentS = data.String()
 	case Mysqlx.ClientMessages_CRUD_UPDATE:
 		var data Mysqlx_Crud.Update
-		proto.Unmarshal(payload, &data)
+		data.Unmarshal(payload)
 		contentS = data.String()
 	case Mysqlx.ClientMessages_CRUD_DELETE:
 		var data Mysqlx_Crud.Delete
-		proto.Unmarshal(payload, &data)
+		data.Unmarshal(payload)
 		contentS = data.String()
 	case Mysqlx.ClientMessages_EXPECT_OPEN:
 		var data Mysqlx_Expect.Open
-		proto.Unmarshal(payload, &data)
+		data.Unmarshal(payload)
 		contentS = data.String()
 	case Mysqlx.ClientMessages_EXPECT_CLOSE:
 		var data Mysqlx_Expect.Close
-		proto.Unmarshal(payload, &data)
+		data.Unmarshal(payload)
 		contentS = data.String()
 	case Mysqlx.ClientMessages_CRUD_CREATE_VIEW:
 		var data Mysqlx_Crud.CreateView
-		proto.Unmarshal(payload, &data)
+		data.Unmarshal(payload)
 		contentS = data.String()
 	case Mysqlx.ClientMessages_CRUD_MODIFY_VIEW:
 		var data Mysqlx_Crud.ModifyView
-		proto.Unmarshal(payload, &data)
+		data.Unmarshal(payload)
 		contentS = data.String()
 	case Mysqlx.ClientMessages_CRUD_DROP_VIEW:
 		var data Mysqlx_Crud.DropView
-		proto.Unmarshal(payload, &data)
+		data.Unmarshal(payload)
 		contentS = data.String()
 	}
-	msg = fmt.Sprintf(msg, typeS, contentS)
+	msg = fmt.Sprintf(msg, typeS, "\"" + contentS + "\"")
 	f.WriteString(msg)
 }
 
@@ -125,64 +124,70 @@ func dealServerMsg(mtype Mysqlx.ServerMessages_Type, payload []byte, f *os.File)
 	var contentS = "NO CONTENT"
 	switch mtype {
 	case Mysqlx.ServerMessages_OK:
-		var ok Mysqlx.Ok
-		proto.Unmarshal(payload, &ok)
-		contentS = ok.String()
+		var data Mysqlx.Ok
+		data.Unmarshal(payload)
+		contentS = data.String()
 	case Mysqlx.ServerMessages_ERROR:
 		var data Mysqlx.Error
-		proto.Unmarshal(payload, &data)
+		data.Unmarshal(payload)
 		contentS = data.String()
 	case Mysqlx.ServerMessages_CONN_CAPABILITIES:
-		var caps Mysqlx_Connection.Capabilities
-		proto.Unmarshal(payload, &caps)
-		contentS = caps.String()
+		var data Mysqlx_Connection.Capabilities
+		data.Unmarshal(payload)
+		contentS = data.String()
 	case Mysqlx.ServerMessages_SESS_AUTHENTICATE_CONTINUE:
-		var authCont Mysqlx_Session.AuthenticateContinue
-		proto.Unmarshal(payload, &authCont)
-		contentS = authCont.String()
+		var data Mysqlx_Session.AuthenticateContinue
+		data.Unmarshal(payload)
+		contentS = data.String()
 	case Mysqlx.ServerMessages_SESS_AUTHENTICATE_OK:
-		var authOk Mysqlx_Session.AuthenticateOk
-		proto.Unmarshal(payload, &authOk)
-		contentS = authOk.String()
+		var data Mysqlx_Session.AuthenticateOk
+		data.Unmarshal(payload)
+		contentS = data.String()
 	case Mysqlx.ServerMessages_NOTICE:
-		var notice Mysqlx_Notice.Frame
-		proto.Unmarshal(payload, &notice)
-		contentS = notice.String()
-		switch notice.GetType() {
+		var data Mysqlx_Notice.Frame
+		data.Unmarshal(payload)
+		contentS = data.String()
+		switch data.GetType() {
 		case 1: // warning
-		case 2: // session veriable changed
+			var data Mysqlx_Notice.Warning
+			data.Unmarshal(payload)
+			contentS += fmt.Sprintf("\"\nNotice Type: Warning, Notice Payload: \"%s", data.String())
+		case 2: // session variable changed
+			var data Mysqlx_Notice.SessionVariableChanged
+			data.Unmarshal(payload)
+			contentS += fmt.Sprintf("\"\nNotice Type: SessionVariableChanged, Notice Payload: \"%s", data.String())
 		case 3: // session state changed
-			var ssc Mysqlx_Notice.SessionStateChanged
-			proto.Unmarshal(payload, &ssc)
-			contentS = fmt.Sprintf("(scope: %s, payload: %s)", notice.GetScope().String(), ssc.String())
+			var data Mysqlx_Notice.SessionStateChanged
+			data.Unmarshal(payload)
+			contentS += fmt.Sprintf("\"\nNotice Type: SessionStateChanged, Notice Payload: \"%s", data.String())
 		}
 	case Mysqlx.ServerMessages_RESULTSET_COLUMN_META_DATA:
-		var rcmd Mysqlx_Resultset.ColumnMetaData
-		proto.Unmarshal(payload, &rcmd)
-		contentS = rcmd.String()
+		var data Mysqlx_Resultset.ColumnMetaData
+		data.Unmarshal(payload)
+		contentS = data.String()
 	case Mysqlx.ServerMessages_RESULTSET_ROW:
-		var row Mysqlx_Resultset.Row
-		proto.Unmarshal(payload, &row)
-		contentS = row.String()
+		var data Mysqlx_Resultset.Row
+		data.Unmarshal(payload)
+		contentS = data.String()
 	case Mysqlx.ServerMessages_RESULTSET_FETCH_DONE:
-		var done Mysqlx_Resultset.FetchDone
-		proto.Unmarshal(payload, &done)
-		contentS = done.String()
+		var data Mysqlx_Resultset.FetchDone
+		data.Unmarshal(payload)
+		contentS = data.String()
 	case Mysqlx.ServerMessages_RESULTSET_FETCH_SUSPENDED:
 	case Mysqlx.ServerMessages_RESULTSET_FETCH_DONE_MORE_RESULTSETS:
 		var data Mysqlx_Resultset.FetchDoneMoreResultsets
-		proto.Unmarshal(payload, &data)
+		data.Unmarshal(payload)
 		contentS = data.String()
 	case Mysqlx.ServerMessages_SQL_STMT_EXECUTE_OK:
-		var sqlOk Mysqlx_Sql.StmtExecuteOk
-		proto.Unmarshal(payload, &sqlOk)
-		contentS = sqlOk.String()
+		var data Mysqlx_Sql.StmtExecuteOk
+		data.Unmarshal(payload)
+		contentS = data.String()
 	case Mysqlx.ServerMessages_RESULTSET_FETCH_DONE_MORE_OUT_PARAMS:
 		var data Mysqlx_Resultset.FetchDoneMoreOutParams
-		proto.Unmarshal(payload, &data)
+		data.Unmarshal(payload)
 		contentS = data.String()
 	}
-	msg = fmt.Sprintf(msg, typeS, contentS)
+	msg = fmt.Sprintf(msg, typeS, "\"" + contentS + "\"")
 	f.WriteString(msg)
 }
 
